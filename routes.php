611395be1e0a3b7f46abbc156f8f6bd5ece3d0f4
Wojtek Pilich <chilip@tlen.pl>
2018-07-20 23:04:40 +0200

    array of routes

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$router->define([
+    'Routing' => 'controllers/index.php',
+    'Routing/about' => 'controllers/about.php',
+    'Routing/about/culture' => 'controllers/about-culture.php',
+    'Routing/contact' => 'controllers/contact.php'
+]);
